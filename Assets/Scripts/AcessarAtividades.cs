59af0954138132900109558edc8e3b81d0040aa5
Warzinhuw <rafael_martorano@hotmail.com>
2022-11-24 22:05:58 -0300

    Menu finalizado e nome do app definido

---
+++ Assets/Scripts/AcessarAtividades.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AcessarAtividades : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CarregarAtividade(string nomeAtividade) {
+        SceneManager.LoadScene(nomeAtividade);
+    }
+}
